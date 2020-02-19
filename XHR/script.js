dbba6b663357085b4c00362484e37a1ed1cb3eec
Danielle Manguele Kathleen <ue2189fu@e2381-as1664963.century.edu>
2020-02-19 11:57:06 -0600

    changes

--- XHR/script.js
+++ XHR/script.js
@@ -1,16 +1,21 @@
 "use strict";
 (function(){
-    var url="https://api.weatherbit.io/v2.0/current?city=yaounde,CAMEROON&key=API_KEY";
+    var url="https://api.weatherbit.io/v2.0/current?city=Yaounde&key=b9169be6ca824b9781734c8c52a01ead";
     var apiKey="b9169be6ca824b9781734c8c52a01ead";
     var httpRequest;
+    makeRequest();
 
     function makeRequest(){
         httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = responseMethod;
         httpRequest.open('GET', url + '&appid=' + apiKey);
         httpRequest.send();
+
     }
     function responseMethod(){
-        if ( httpRequest.readyState===4)
+        if ( httpRequest.readyState===4){
+            console.log(httpRequest.responseText);
+        }
     }
 
 })();
